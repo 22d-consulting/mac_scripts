aa666067ceada5c04d2a898ab8d7316f8332a002
Dominik Kugelmann <dominik@22dconsulting.com>
2022-02-11 22:04:25 +0100

    Update and rename available_gsuite_licenses.sh to available_workspace_licenses.sh

--- available_workspace_licenses.sh
+++ available_workspace_licenses.sh
@@ -8,21 +8,24 @@ gam="$HOME/bin/gam/gam"
 slack_webhook_url="https://hooks.slack.com/services/..."
 # Set threshold to send slack notifications if less then this
 license_threshold="5"
+# Set Workspace Edition.  See: https://github.com/taers232c/GAMADV-XTD3/wiki/Reseller#definitions 
+workspace_edition="workspaceenterprisestandard"
+customer_domain="google.com"
 
 # End of settings that need configured
 
 echo "License threshold set to" $license_threshold". Slack notification will be sent if available licenses is less than this."
-# Find license count and user count for G Suite Enterprise Licenses
-license_count=$($gam info domain | grep 'G Suite Enterprise Licenses' | tr -cd '0-9')
-user_count=$($gam print license sku enterprise countsonly | tail -c 4)
+# Find license count and user count for licenses specified.  Use 'numberOfSeats:' for commitments & 'maximumNumberOfSeats:' for flex licenses.
+license_count=$($gam info resoldsubscriptions $customer_domain $workspace_edition | grep 'numberOfSeats:' | tr -cd '0-9')
+user_count=$($gam info resoldsubscriptions $customer_domain $workspace_edition | grep 'licensedNumberOfSeats:' | tr -cd '0-9')
 
 # Calculate and display available G Suite user licenses, then store in variable
 available_licenses=$(expr $license_count - $user_count)
-echo $available_licenses "G Suite Licenses Available"
+echo $available_licenses "Google $workspace_edition Licenses Available"
 
 if [ $available_licenses -lt $license_threshold ] 
 then
-    curl -X POST -H 'Content-type: application/json' --data "$(eval "echo "'{\"text\": \"$available_licenses G Suite Licenses Available\"}'"")" $slack_webhook_url
+    curl -X POST -H 'Content-type: application/json' --data "$(eval "echo "'{\"text\": \"$available_licenses Google $workspace_edition Licenses Available\"}'"")" $slack_webhook_url
 else
     echo $available_licenses "Available licenses exceeds the set threshold of" $license_threshold". No Slack notification will be sent"
 fi
